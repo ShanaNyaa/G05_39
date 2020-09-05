a55731484c3208e1312ada8d50c05e04de8340e0
Danial <nizwien@gmail.com>
2020-09-05 18:43:54 +0800

    Add redirection for car

---
+++ processCar.php
@@ -0,0 +1,15 @@
+<?php
+    include "car.php";
+
+    if(isSet($_POST["deleteButton"])) {
+		deleteCar();
+	}
+
+	if(isSet($_POST["updateButton"])) {
+		updateCar();
+    }
+
+    if(isSet($_POST["addButton"])) {
+		addCar();
+	}
+?>
