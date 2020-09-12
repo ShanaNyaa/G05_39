7f1c99a17c3ec064177157a33e9c222d57177830
Danial <nizwien@gmail.com>
2020-09-12 18:57:13 +0800

    Redesign website

--- adminCarList.php
+++ adminCarList.php
@@ -1,97 +1,295 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
+    <title>Admin Car List Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+@media(min-width:768px) {
     body {
-        background: url("assets/img/hero-bg.jpg") no-repeat center center fixed;
-        background-size: cover;
+        margin-top: 50px;
     }
-</style>
+    /*html, body, #wrapper, #page-wrapper {height: 100%; overflow: hidden;}*/
+}
+
+#wrapper {
+    padding-left: 0;    
+}
+
+#page-wrapper {
+    width: 100%;        
+    padding: 0;
+    background-color: #fff;
+}
+
+@media(min-width:768px) {
+    #wrapper {
+        padding-left: 225px;
+    }
+
+    #page-wrapper {
+        padding: 22px 10px;
+    }
+}
+
+/* Top Navigation */
+
+.top-nav {
+    padding: 0 15px;
+}
+
+.top-nav>li {
+    display: inline-block;
+    float: left;
+}
+
+.top-nav>li>a {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    line-height: 20px;
+    color: #fff;
+}
+
+.top-nav>li>a:hover,
+.top-nav>li>a:focus,
+.top-nav>.open>a,
+.top-nav>.open>a:hover,
+.top-nav>.open>a:focus {
+    color: #fff;
+    background-color: #1a242f;
+}
+
+.top-nav>.open>.dropdown-menu {
+    float: left;
+    position: absolute;
+    margin-top: 0;
+    /*border: 1px solid rgba(0,0,0,.15);*/
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    background-color: #fff;
+    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+    box-shadow: 0 6px 12px rgba(0,0,0,.175);
+}
+
+.top-nav>.open>.dropdown-menu>li>a {
+    white-space: normal;
+}
+
+/* Side Navigation */
+
+@media(min-width:768px) {
+    .side-nav {
+        position: fixed;
+        top: 60px;
+        left: 225px;
+        width: 225px;
+        margin-left: -225px;
+        border: none;
+        border-radius: 0;
+        border-top: 1px rgba(0,0,0,.5) solid;
+        overflow-y: auto;
+        background-color: #222;
+        /*background-color: #5A6B7D;*/
+        bottom: 0;
+        overflow-x: hidden;
+        padding-bottom: 40px;
+    }
+
+    .side-nav>li>a {
+        width: 225px;
+        border-bottom: 1px rgba(0,0,0,.3) solid;
+    }
+
+    .side-nav li a:hover,
+    .side-nav li a:focus {
+        outline: none;
+        background-color: #1a242f !important;
+    }
+}
+
+.side-nav>li>ul {
+    padding: 0;
+    border-bottom: 1px rgba(0,0,0,.3) solid;
+}
+
+.side-nav>li>ul>li>a {
+    display: block;
+    padding: 10px 15px 10px 38px;
+    text-decoration: none;
+    /*color: #999;*/
+    color: #fff;    
+}
+
+.side-nav>li>ul>li>a:hover {
+    color: #fff;
+}
+
+.navbar .nav > li > a > .label {
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  position: absolute;
+  top: 14px;
+  right: 6px;
+  font-size: 10px;
+  font-weight: normal;
+  min-width: 15px;
+  min-height: 15px;
+  line-height: 1.0em;
+  text-align: center;
+  padding: 2px;
+}
+
+.navbar .nav > li > a:hover > .label {
+  top: 10px;
+}
+
+.navbar-brand {
+    padding: 5px 15px;
+}
+    </style>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <?php
+    session_start();
+    if(!isset($_SESSION["email"]) && $_SESSION["password"] != "") {
+        header("location: index.html");
+    }
+	
+	include "user.php";
+    $userID = getUserID();
+    
     include "car.php";
     $carList = getListOfCar();
+    $noOfCar = mysqli_num_rows($carList);
+    $bil = 1;
 
-    if (isSet($_POST["searchByPlateNumber"])) {
-        $carList = searchCarByPlateNumber();
-    }
-
-    if (isSet($_POST["searchByName"])) {
-		$carList = searchCarByName();
-	}
-	
-	if (isSet($_POST["searchByBrand"])) {
-		$carList = searchCarByBrand();
-	}
-	
-	if (isSet($_POST["searchByColour"])) {
-		$carList = searchCarByColour();
-    }
-    
-    if (isSet($_POST["searchByYear"])) {
-        $carList = searchCarByYear();
-    }
-	
-	if (isSet($_POST["displayAllButton"])) {
-		$carList = getListOfCar();
-	}
-	
-	if (isSet($_POST["addCar"])) {
+    if (isSet($_POST["addCar"])) {
 		header("location: addCar.php");
 	}
-
-    $noOfCar = mysqli_num_rows($carList);
-    echo '<div class="container-fluid">';
-    displaySearchPanel();
-    echo '<p style="color: white; text-shadow: 2px 2px 5px #000000">There are ' .$noOfCar. ' car(s)</p>';
-	echo "<table class='table table-dark table-hover'>";
-	$bil = 1;
-	echo "<tr><th>#</th><th>Plate Number</th><th>Name</th><th>Brand</th><th>Colour</th><th>Year</th>";
-	while($row = mysqli_fetch_assoc($carList)) {
-        echo "<tr>";
-        $plateNumber = $row["plateNumber"];
-            echo "<td>".$bil. "</td>";
-            echo "<td>".$row["plateNumber"]."</td>";
-            echo "<td>".$row["carName"]."</td>";
-            echo "<td>".$row["brand"]."</td>";
-            echo "<td>".$row["colour"]."</td>";
-            echo "<td>".$row["year"]."</td>";
-            echo "<td>";
-                echo '<form action="updateCarForm.php" method="POST">';
-                echo "<input type='hidden' value='$plateNumber' name='plateNumberToUpdate'>";
-                echo '<button type="submit" class="btn btn-primary" name="updateButton">Update</button>';
-                echo "</form>";
-            echo "</td>";
-            echo "<td>";
-                echo '<form action="processCar.php" method="POST">';
-                echo "<input type='hidden' value='$plateNumber' name='plateNumberToDelete'>";
-                echo '<button type="submit" class="btn btn-danger" name="deleteButton">Delete</button>';
-                echo "</form>";
-            echo "</td>";
-        echo "</tr>";
-        $bil++;
-        echo '</div>';
-	}
-    echo "</table>";
-    
-    function displaySearchPanel() {
-        echo '<form style="margin-top: 5px" action="adminCarList.php" method="POST">';
-        echo '<p style="color: white; text-shadow: 2px 2px 5px #000000">Search Car: ';
-        echo '<input class="form mr-sm-2" type="text" name="searchValue">';
-        echo '<button type="submit" class="btn btn-primary mr-sm-2" name="searchByPlateNumber">Search By Plate Number</button>';
-        echo '<button type="submit" class="btn btn-dark mr-sm-2" name="searchByName">Search By Name</button>';
-        echo '<button type="submit" class="btn btn-secondary mr-sm-2" name="searchByBrand">Search By Brand</button>';
-        echo '<button type="submit" class="btn btn-info mr-sm-2" name="searchByColour">Search By Colour</button>';
-        echo '<button type="submit" class="btn btn-success mr-sm-2" name="searchByYear">Search By Year</button>';
-        echo '<button type="submit" class="btn btn-danger mr-sm-2" name="displayAllButton">Display All</button>';
-        echo '<button type="submit" class="btn btn-light mr-sm-2" name="addCar">Add Car</button></p>';
-        echo '</form>';
-    }
 ?>
+</head>
+<body>
+<div id="throbber" style="display:none; min-height:120px;"></div>
+<div id="noty-holder"></div>
+<div id="wrapper">
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="adminHomepage.php">
+                <img src="http://placehold.it/200x50&text=HOME" alt="LOGO">
+            </a>
+        </div>
+        <!-- Top Menu Items -->
+        <ul class="nav navbar-right top-nav">           
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin User <b class="fa fa-angle-down"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="profile.php"><i class="fa fa-fw fa-user"></i> Edit Profile</a></li>
+                    <li class="divider"></li>
+                    <li><a href="index.html"><i class="fa fa-fw fa-power-off"></i> Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                <li>
+                    <a href="memberList.php"><i class="fa fa-fw fa-user"></i> Member List</a>
+                </li>
+                <li>
+                    <a href="adminCarList.php"><i class="fa fa-fw fa-car"></i> Car List</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row" id="main">
+                <div class="col-sm-12 col-md-12 well well-sm" id="content">
+                <div class="form-group">
+                    <input class="form-control" id="tableSearch" type="text" placeholder="Type something to search">
+                </div>
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th><th>Plate Number</th><th>Name</th><th>Brand</th><th>Colour</th><th>Year</th>
+                                <th colspan="2" class="text-center">
+                                    <form action="adminCarList.php" method="POST">
+                                        <button type="submit" class="btn btn-primary" name="addCar">Add Car</button>
+                                    </form>
+                                </th>
+                                <tbody id="myTable">
+                                    <?php
+                                        while($row = mysqli_fetch_assoc($carList)) {
+                                            echo "<tr>";
+                                            $plateNumber = $row["plateNumber"];
+                                                echo "<td>".$bil. "</td>";
+                                                echo "<td>".$row["plateNumber"]."</td>";
+                                                echo "<td>".$row["carName"]."</td>";
+                                                echo "<td>".$row["brand"]."</td>";
+                                                echo "<td>".$row["colour"]."</td>";
+                                                echo "<td>".$row["year"]."</td>";
+                                                echo "<td class='text-center'>";
+                                                    echo '<form action="updateCarForm.php" method="POST">';
+                                                    echo "<input type='hidden' value='$plateNumber' name='plateNumberToUpdate'>";
+                                                    echo '<button type="submit" class="btn btn-primary" name="updateButton">Update</button>';
+                                                    echo "</form>";
+                                                echo "</td>";
+                                                echo "<td class='text-center'>";
+                                                    echo '<form action="processCar.php" method="POST">';
+                                                    echo "<input type='hidden' value='$plateNumber' name='plateNumberToDelete'>";
+                                                    echo '<button type="submit" class="btn btn-danger" name="deleteButton">Delete</button>';
+                                                    echo "</form>";
+                                                echo"</td>";
+                                                echo "</tr>";
+                                            $bil++;
+                                        }
+                                    ?>
+                                </tbody>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+</div><!-- /#wrapper -->
+<script type="text/javascript">
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".side-nav .collapse").on("hide.bs.collapse", function() {                   
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+    });
+    $('.side-nav .collapse').on("show.bs.collapse", function() {                        
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");        
+    });
+    // Filter table
+    $(document).ready(function(){
+    $("#tableSearch").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    });
+})    
+</script>
+</body>
+</html>
