d96c2b1ed9eb256d2925138f457d164f66046935
Danial <nizwien@gmail.com>
2020-10-03 03:45:37 +0800

    Remove unnecessary stuff and replace user icon

--- profile.php
+++ profile.php
@@ -49,7 +49,7 @@
 		echo "<h1>".$username."</h1>";
 		?>
 		</div>
-    	<div class="col-sm-2"><a href="/users" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="http://www.gravatar.com/avatar/28fd20ccec6865e2d5f0e1f4446eb7bf?s=100"></a></div>
+    	<div class="col-sm-2"><a href="" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="assets\img\round_account_circle_black_48dp.png"></a></div>
     </div>
     <div class="row">
   		<div class="col-sm-3"><!--left col-->
@@ -65,27 +65,6 @@
 		</form>
 		</div></hr><br>
 
-               
-        <div class="panel panel-default">
-            <div class="panel-heading">Website <i class="fa fa-link fa-1x"></i></div>
-            <div class="panel-body"><a href="http://bootnipets.com">bootnipets.com</a></div>
-        </div>
-          
-          
-        <ul class="list-group">
-            <li class="list-group-item text-muted">Activity <i class="fa fa-dashboard fa-1x"></i></li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Shares</strong></span> 125</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Likes</strong></span> 13</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Posts</strong></span> 37</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Followers</strong></span> 78</li>
-        </ul> 
-               
-        <div class="panel panel-default">
-            <div class="panel-heading">Social Media</div>
-			<div class="panel-body">
-            	<i class="fa fa-facebook fa-2x"></i> <i class="fa fa-github fa-2x"></i> <i class="fa fa-twitter fa-2x"></i> <i class="fa fa-pinterest fa-2x"></i> <i class="fa fa-google-plus fa-2x"></i>
-            </div>
-        </div> 
         </div><!--/col-3-->
     	<div class="col-sm-9">
             <ul class="nav nav-tabs">
