03f2dfaa90d951eb7a1ce9fb28d8ff53b73484f7
Danial <nizwien@gmail.com>
2020-10-05 11:09:50 +0800

    Redesign UI

--- updateUserForm.php
+++ updateUserForm.php
@@ -1,47 +1,259 @@
 <!DOCTYPE html>
-<html>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
+    <title>Admin Update User Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+@media(min-width:768px) {
+    body {
+        margin-top: 50px;
+    }
+    /*html, body, #wrapper, #page-wrapper {height: 100%; overflow: hidden;}*/
+}
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+#wrapper {
+    padding-left: 0;    
+}
 
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+#page-wrapper {
+    width: 100%;        
+    padding: 0;
+    background-color: #fff;
+}
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+@media(min-width:768px) {
+    #wrapper {
+        padding-left: 225px;
+    }
 
-<style>
-	body {
-		background: url("assets/img/hero-bg.jpg") no-repeat center center fixed;
-		background-size: cover;
-	}
-</style>
+    #page-wrapper {
+        padding: 22px 10px;
+    }
+}
+
+/* Top Navigation */
+
+.top-nav {
+    padding: 0 15px;
+}
+
+.top-nav>li {
+    display: inline-block;
+    float: left;
+}
+
+.top-nav>li>a {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    line-height: 20px;
+    color: #fff;
+}
+
+.top-nav>li>a:hover,
+.top-nav>li>a:focus,
+.top-nav>.open>a,
+.top-nav>.open>a:hover,
+.top-nav>.open>a:focus {
+    color: #fff;
+    background-color: #1a242f;
+}
+
+.top-nav>.open>.dropdown-menu {
+    float: left;
+    position: absolute;
+    margin-top: 0;
+    /*border: 1px solid rgba(0,0,0,.15);*/
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    background-color: #fff;
+    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+    box-shadow: 0 6px 12px rgba(0,0,0,.175);
+}
+
+.top-nav>.open>.dropdown-menu>li>a {
+    white-space: normal;
+}
+
+/* Side Navigation */
+
+@media(min-width:768px) {
+    .side-nav {
+        position: fixed;
+        top: 60px;
+        left: 225px;
+        width: 225px;
+        margin-left: -225px;
+        border: none;
+        border-radius: 0;
+        border-top: 1px rgba(0,0,0,.5) solid;
+        overflow-y: auto;
+        background-color: #222;
+        /*background-color: #5A6B7D;*/
+        bottom: 0;
+        overflow-x: hidden;
+        padding-bottom: 40px;
+    }
+
+    .side-nav>li>a {
+        width: 225px;
+        border-bottom: 1px rgba(0,0,0,.3) solid;
+    }
+
+    .side-nav li a:hover,
+    .side-nav li a:focus {
+        outline: none;
+        background-color: #1a242f !important;
+    }
+}
+
+.side-nav>li>ul {
+    padding: 0;
+    border-bottom: 1px rgba(0,0,0,.3) solid;
+}
+
+.side-nav>li>ul>li>a {
+    display: block;
+    padding: 10px 15px 10px 38px;
+    text-decoration: none;
+    /*color: #999;*/
+    color: #fff;    
+}
+
+.side-nav>li>ul>li>a:hover {
+    color: #fff;
+}
+
+.navbar .nav > li > a > .label {
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  position: absolute;
+  top: 14px;
+  right: 6px;
+  font-size: 10px;
+  font-weight: normal;
+  min-width: 15px;
+  min-height: 15px;
+  line-height: 1.0em;
+  text-align: center;
+  padding: 2px;
+}
+
+.navbar .nav > li > a:hover > .label {
+  top: 10px;
+}
+
+.navbar-brand {
+    padding: 5px 15px;
+}
+    </style>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <?php
-include "user.php";
-$qry = getMemberInformation();
-$row = mysqli_fetch_assoc($qry);
+    session_start();
+    if(!isset($_SESSION["email"]) && $_SESSION["password"] != "") {
+        header("location: index.html");
+    }
+	
+	include "user.php";
+	$qry = getMemberInformation();
+	$row = mysqli_fetch_assoc($qry);
 
-$userID = $row["userID"];
-$email = $row["email"];
-$password = $row["password"];
-$username = $row["username"];
-$contactNumber = $row["contactNumber"];
-
-echo '<div class="container" style="text-align: center; margin-top: 10%; padding-right: 50px; padding-left: 50px; padding-top: 5px; width: 650px">';
-echo '<h1 style="color: white; text-shadow: 2px 2px 5px #000000">Edit Account</h1>';
-	echo '<div class="card bg-dark">';
-		echo '<div class="card-body text-white" style="box-shadow: 2px 2px 5px #000000">';
-			echo '<form action="processProfile.php" method="post">';
-				echo '<p style="text-shadow: 2px 2px 5px #000000; font-size: 24px; padding-left: 4px">User ID: <input class="text-muted" type="text" name="userID" value="'.$userID.'" readonly>';
-				echo '<p style="text-shadow: 2px 2px 5px #000000; font-size: 24px; padding-left: 25px">Email: <input type="email" name="email" value="'.$email.'">';
-				echo '<p style="text-shadow: 2px 2px 5px #000000; font-size: 24px; padding-right: 18px">Password: <input type="password" name="password" value="'.$password.'">';
-				echo '<p style="text-shadow: 2px 2px 5px #000000; font-size: 24px; padding-right: 23px">Username: <input type="text" name="username" value="'.$username.'">';
-				echo '<p style="text-shadow: 2px 2px 5px #000000; font-size: 24px; padding-left: 0px">Contact: <input type="text" name="contactNumber" value="'.$contactNumber.'">';;
-				echo '<button type="submit" class="btn btn-primary" style="width: 85%; margin-top: 25px" name="updateButton">Update</button>';
-			echo '</form>';
-		echo '</div>';
-	echo '</div>';
-echo '</div>';
+	$userID = $row["userID"];
+	$email = $row["email"];
+	$password = $row["password"];
+	$username = $row["username"];
+	$contactNumber = $row["contactNumber"];
 ?>
+</head>
+<body>
+<div id="throbber" style="display:none; min-height:120px;"></div>
+<div id="noty-holder"></div>
+<div id="wrapper">
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="adminHomepage.php">
+                <img src="http://placehold.it/200x50&text=HOME" alt="LOGO">
+            </a>
+        </div>
+        <!-- Top Menu Items -->
+        <ul class="nav navbar-right top-nav">           
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin User <b class="fa fa-angle-down"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="profile.php"><i class="fa fa-fw fa-user"></i> Edit Profile</a></li>
+                    <li class="divider"></li>
+                    <li><a href="index.html"><i class="fa fa-fw fa-power-off"></i> Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                <li>
+                    <a href="userList.php"><i class="fa fa-fw fa-user"></i> User List</a>
+                </li>
+                <li>
+                    <a href="adminCarList.php"><i class="fa fa-fw fa-car"></i> Car List</a>
+                </li>
+                <li>
+                    <a href="adminBookingList.php"><i class="fa fa-fw fa-book"></i> Booking List</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row" id="main">
+                <div class="col-sm-12 col-md-12 well well-sm">
+                    <h1>Update Account</h1>
+				</div>
+                <div class="container">
+                    <div class="col-sm-12 col-md-12 well well-sm text-center">
+                        <form action="processProfile.php" method="POST">
+							<?php
+                            echo '<p style="font-size: 24px; padding-left: 14px">User ID: <input class="text-muted" type="text" name="userID" value="'.$userID.'" readonly>';
+                            echo '<p style="font-size: 24px; padding-left: 36px">Email: <input type="email" name="email" value="'.$email.'">';
+                            echo '<p style="font-size: 24px; padding-right: 10px">Password: <input type="password" name="password" value="'.$password.'">';
+                            echo '<p style="font-size: 24px; padding-right: 16px">Username: <input type="text" name="username" value="'.$username.'">';
+							echo '<p style="font-size: 24px; padding-left: 12px">Contact: <input type="text" name="contactNumber" value="'.$contactNumber.'">';
+							echo '</p>';
+							?>
+							<button type="submit" class="btn btn-primary" style="font-size: 24px; width: 40%" name="updateButton">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+</div><!-- /#wrapper -->
+<script type="text/javascript">
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".side-nav .collapse").on("hide.bs.collapse", function() {                   
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+    });
+    $('.side-nav .collapse').on("show.bs.collapse", function() {                        
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");        
+    }); 
+</script>
+</body>
+</html>
